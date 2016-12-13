ef27519dcc667b8823cfe99548aa9fa129649367
pingwarrior <pingwarrior@Koays-MacBook-Pro.local>
2016-12-13 23:05:49 +0800

    Initial
    First Upload

---
+++ module00/00-prep-03-python-primer2/nested_datatypes.py
@@ -0,0 +1,13 @@
+food={"vegetables":["carrots","kale","cucumber","tomato"],"desserts":["cake","ice cream", "donut"]}
+print("My favorite vegetable is " + food["vegetables"][0])
+print("My favorite vegetable is " + food["vegetables"][1])
+
+cars={"sports":{"Volkswagon":"Porsche","Dodge":"Viper","Chevy":"Corvette"},"classic":{"Mercedes-Benz":"300SL","Toyota":"2000GT","Lincoln":"Continental"}}
+print("My favorite sports car is a " + cars["sports"]["Dodge"])
+print("My favorite sports car is a " + cars["sports"]["Chevy"])
+
+
+dessert={"iceCream":["Rocky Road","strawberry","Pistachio Cashew","Pecan Praline"]}
+
+
+soup={"soup":{"tomato":"healthy","onion":"bleh!","vegetable":"good for you"}}
---
+++ module00/00-prep-03-python-primer2/nested_datatypes_sol.py
@@ -0,0 +1,35 @@
+food={"vegetables":["carrots","kale","cucumber","tomato"],"desserts":["cake","ice cream", "donut"]}
+strVeg = "My favorite vegetable is "
+strDes = "My favorite dessert is "
+print(strVeg + food["vegetables"][0])
+print(strVeg + food["vegetables"][1])
+print(strVeg + food["vegetables"][2])
+print(strVeg + food["vegetables"][3])
+print(strDes + food["desserts"][0])
+print(strDes + food["desserts"][1])
+print(strDes + food["desserts"][2])
+
+cars={"sports":{"Volkswagon":"Porsche","Dodge":"Viper","Chevy":"Corvette"},"classic":{"Mercedes-Benz":"300SL","Toyota":"2000GT","Lincoln":"Continental"}}
+strSport = "My favorite sports car is a "
+strClassic = "My favorite classic car is a "
+print(strSport + cars["sports"]["Dodge"])
+print(strSport + cars["sports"]["Chevy"])
+print(strSport + cars["sports"]["Volkswagon"])
+print(strClassic + cars["classic"]["Mercedes-Benz"])
+print(strClassic + cars["classic"]["Toyota"])
+print(strClassic + cars["classic"]["Lincoln"])
+
+
+dessert={"iceCream":["Rocky Road","strawberry","Pistachio Cashew","Pecan Praline"]}
+strCream = "My favorite ice cream is "
+print(strCream + dessert["iceCream"][0])
+print(strCream + dessert["iceCream"][1])
+print(strCream + dessert["iceCream"][2])
+print(strCream + dessert["iceCream"][3])
+
+
+soup={"soup":{"tomato":"healthy","onion":"bleh!","vegetable":"good for you"}}
+strSoup = "This soup is "
+print(strSoup + soup["soup"]["tomato"])
+print(strSoup + soup["soup"]["onion"])
+print(strSoup + soup["soup"]["vegetable"])
